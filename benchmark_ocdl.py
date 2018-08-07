ab7b63e72e68a8787b53616b3daa96d82a4781d9
demonzyj56 <demonzyj56@gmail.com>
2018-08-07 16:29:55 +0800

    Modify

--- benchmark_ocdl.py
+++ benchmark_ocdl.py
@@ -11,6 +11,7 @@ import argparse
 import datetime
 import logging
 import os
+import pprint
 import sys
 import yaml
 import pyfftw  # pylint: disable=unused-import
@@ -21,12 +22,10 @@ from matplotlib.ticker import MaxNLocator
 import torch
 import sporco.util as su
 import sporco.metric as sm
-from sporco.admm import cbpdn
 from sporco.dictlrn.onlinecdl import OnlineConvBPDNDictLearn
 from fista_slice_online import OnlineDictLearnSliceSurrogate
 from onlinecdl_surrogate import OnlineDictLearnDenseSurrogate
-import image_dataset
-from cifar import CIFAR10
+from datasets import get_dataset, BlobLoader
 
 
 def parse_args():
@@ -43,7 +42,7 @@ def parse_args():
     parser.add_argument('--num_atoms', default=100, type=int, help='Size of dictionary')
     parser.add_argument('--rng_seed', default=-1, type=int, help='Random seed to use; negative values mean dont set')
     parser.add_argument('--no_tikhonov_filter', action='store_true', help='No tikhonov low pass filtering is applied')
-    parser.add_argument('--skip_test', action='store_true', help='Skip running reconstruction')
+    parser.add_argument('--use_gray', action='store_true', help='Use grayscale image instead of color image')
     return parser.parse_args()
 
 
@@ -67,81 +66,25 @@ def setup_logging(name, filename=None):
 
 def train_models(solvers, train_blob, args):
     """Function for training every solvers."""
-    cur = 0
-    idx = np.random.permutation(train_blob.shape[-1])
-    # setup for every solver
-    paths = {}
-    for name in solvers.keys():
-        path = os.path.join(args.output_path, name)
-        if not os.path.exists(path):
-            os.makedirs(path)
-        paths.update({name: path})
-    for e in range(args.epochs):
-        if cur + args.batch_size > len(idx):
-            cur = 0
-            idx = np.random.permutation(train_blob.shape[-1])
-        # step all solvers to ensure that for each step they receive
-        # same data
+    if args.use_gray:
+        D0 = np.random.randn(args.patch_size, args.patch_size, args.num_atoms)
+    else:
+        D0 = np.random.randn(args.patch_size, args.patch_size, 3, args.num_atoms)
+
+    loader = BlobLoader(train_blob, args.epochs, args.batch_size)
+    sample = loader.random_sample()
+    solvers = {k: sol_name(D0, sample, args.lmbda, opt=opt) for
+               k, (sol_name, opt) in solvers.items()}
+    for e, blob in enumerate(loader):
+        if not args.no_tikhonov_filter:
+            # fix lambda to be 5
+            _, blob = su.tikhonov_filter(blob, 5.)
         for k, s in solvers.items():
-            blob = train_blob[..., idx[cur:cur+args.batch_size]]
-            if not args.no_tikhonov_filter:
-                # fix lambda to be 5
-                _, blob = su.tikhonov_filter(blob, 5.)
             s.solve(blob)
-            np.save(os.path.join(paths[k], f'{args.dataset}.{e}.npy'),
+            path = os.path.join(args.output_path, k)
+            np.save(os.path.join(path, f'{args.dataset}.{e}.npy'),
                     s.getdict().squeeze())
-        cur += args.batch_size
     return solvers
-
-
-def reconstruct_psnr(solvers, test_blob, args, opt=None):
-    """Compute reonstruction PSNR on test samples."""
-    logger = logging.getLogger(__name__)
-    if not args.no_tikhonov_filter:
-        sl, sh = su.tikhonov_filter(test_blob, 5.)
-    else:
-        sl = 0.
-        sh = test_blob
-    psnr_all = {}
-    for k, v in solvers.items():
-        d = cbpdn.ConvBPDN(v.getdict().squeeze(), sh, args.lmbda, opt=opt)
-        d.solve()
-        shr = d.reconstruct().squeeze()
-        imgr = sl + shr
-        psnr = sm.psnr(test_blob, imgr)
-        logger.info('Reconstruction PSNR for {:s}: {:.3f}dB'.format(k, psnr))
-        psnr_all.update({k: psnr})
-    return psnr_all
-
-
-def dataset_loader(name, args):
-    """Return train and test dataset by name."""
-    if name == 'fruit' or name == 'city':
-        train_blob = image_dataset.create_image_blob(name, np.float32,
-                                                     scaled=True, gray=False)
-        test_blob = image_dataset.create_image_blob('singles', np.float32,
-                                                    scaled=True, gray=False)
-    elif name == 'fruit.gray' or name == 'city.gray':
-        train_path = os.path.join('images', 'OCSC', name.split('.')[0]+'_10',
-                                  'train', 'train_lcne.mat')
-        test_path = os.path.join('images', 'OCSC', name.split('.')[0]+'_10',
-                                 'test', 'test_lcne.mat')
-        assert os.path.exists(train_path) and os.path.exists(test_path)
-        train_blob = sio.loadmat(train_path)['b'].astype(np.float32)
-        test_blob = sio.loadmat(test_path)['b'].astype(np.float32)
-    elif name == 'cifar10':
-        cifar10_train = CIFAR10(root='.cifar10', train=True, download=True,
-                                data_type=np.float32)
-        cifar10_test = CIFAR10(root='.cifar10', train=False, download=True,
-                               data_type=np.float32)
-        train_blob = cifar10_train.train_data / 255.
-        test_blob = cifar10_test.test_data / 255.
-        train_blob = train_blob.transpose(2, 3, 1, 0)
-        test_blob = test_blob.transpose(2, 3, 1, 0)
-        del cifar10_train, cifar10_test
-    else:
-        raise NotImplementedError
-    return (train_blob, test_blob)
 
 
 def plot_and_save_statistics(solvers, args):
@@ -154,11 +97,11 @@ def plot_and_save_statistics(solvers, args):
             plt.imshow(su.tiledict(d), cmap='gray')
         else:
             plt.imshow(su.tiledict(d))
-        plt.savefig(os.path.join(args.output_path, f'{args.dataset}.{k}.pdf'),
+        plt.savefig(os.path.join(args.output_path, k, f'{args.dataset}.pdf'),
                     bbox_inches='tight')
         # save statistics
         stats_arr = su.ntpl2array(v.getitstat())
-        np.save(os.path.join(args.output_path, f'{args.dataset}.{k}_stats.npy'),
+        np.save(os.path.join(args.output_path, k, f'{args.dataset}.npy'),
                 stats_arr)
     if 1:
         plt.clf()
@@ -188,6 +131,7 @@ def main():
         )
     )
     logger = setup_logging(__name__, log_name)
+    logger.info(pprint.pformat(args))
 
     # set random seed
     if args.rng_seed >= 0:
@@ -196,26 +140,22 @@ def main():
 
     # load configs
     cfg = yaml.load(open(args.cfg, 'r'))
-    copt = cbpdn.ConvBPDN.Options(cfg['ConvBPDN'])
     del cfg['ConvBPDN']
+    logger.info('cfg:')
+    logger.info(pprint.pformat(cfg))
 
     # load data
-    train_blob, test_blob = dataset_loader(args.dataset, args)
-
-    # load dictionary
-    if train_blob.ndim == 3:
-        D0 = np.random.randn(args.patch_size, args.patch_size, args.num_atoms)
-    else:
-        D0 = np.random.randn(args.patch_size, args.patch_size, 3, args.num_atoms)
+    train_blob = get_dataset(args.dataset, gray=args.use_gray, train=True)
 
     # load solvers
     solvers = {}
     for k, v in cfg.items():
         solver_class = globals()[k.split('-')[0]]
         opt = solver_class.Options(v)
-        solvers.update({
-            k: solver_class(D0, train_blob[..., [0]], args.lmbda, opt=opt)
-        })
+        path = os.path.join(args.output_path, k)
+        if not os.path.exists(path):
+            os.makedirs(path)
+        solvers.update({k: (solver_class, opt)})
 
     # train solvers
     solvers = train_models(solvers, train_blob, args)
